4fa3c935577ad96ab5220f07634f00f6e015e332
Hoang Thi Thu Huyen <huyenhtt1.bhsoft@gmail.com>
2025-04-25 13:51:31 +0700

    update report after change

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,6 +28,9 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
     headless: true,
+    launchOptions: {
+      args: ['--no-sandbox'],
+    },
     viewport: { width: 1280, height: 720 },
     screenshot: "only-on-failure",
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
